72eefb832c00412780ad0055c38a3c2352735115
like <ke.li@yuweioptica.com>
2025-04-07 14:37:12 +0800

    move usage of gauss laguerre to simulation

--- tests/test_gauss_laguerre.cpp
+++ tests/test_gauss_laguerre.cpp
@@ -22,43 +22,6 @@ void test(size_t xsize, size_t ysize, T sigma, int order, int associate)
     imshow(vec, {xsize, ysize});
 }
 
-template<class T, class FuncConvWithKernel> std::vector<std::vector<T>> gauss_laguerre_conv_linear(const std::array<size_t, 2> shape, T sigma, size_t max_associated_order, size_t max_laguerre_order, FuncConvWithKernel&& conv_image_with)
-{
-    std::vector<std::vector<T>> linears;
-    linears.reserve(max_laguerre_order * max_associated_order);
-    for(size_t laguerre_order = 0; laguerre_order < max_laguerre_order; laguerre_order++)
-    for(size_t associated_order = 0; associated_order < max_associated_order; associated_order++)
-    {
-        std::vector<T> kernel(shape[0] * shape[1]);
-        kernels::gauss_laguerre<T, 2, false>(kernel.data(), shape, sigma, laguerre_order, associated_order);
-        conv_image_with(kernel);
-        linears.push_back(std::move(kernel));
-    }
-}
-template<class T> inline std::vector<std::vector<T>> gauss_laguerre_conv_quadratic(const std::vector<std::vector<T>>& linear_results)
-{
-    std::vector<std::vector<T>> quadratic_result;
-    size_t N = linear_results.size();
-    quadratic_result.reserve(N * (N + 1) / 2);
-    // 对称正定矩阵，只用计算上三角部分
-    for(int r = 0; r < N; r++)
-    for(int c = r; c < N; c++){
-        std::vector<T> prod;
-        auto& a = linear_results.at(r);
-        auto& b = linear_results.at(c);
-        std::transform(a.begin(), a.end(), b.begin(), std::back_insert_iterator(prod), [](T l, T r){return l * std::conj(r);});
-        quadratic_result.push_back(std::move(prod));
-    }
-    return quadratic_result;
-}
-template<class T> std::vector<T> quadratic_coefficients_diagonalize(const std::vector<T>& coef_of_quad, size_t eigen_value_count)
-{
-    std::vector<T> eigenval(eigen_value_count);
-    LAPACKE_theev(LAPACK_COL_MAJOR, 'V', 'U', eigen_value_count, coef_of_quad.data(), eigen_value_count, eigenval.data());
-    return eigenval;
-}
-
-
 int main()
 {
     py_engine::init();
