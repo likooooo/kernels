c352e28a1978d14cfca220a752cd3c52591b9357
like <ke.li@yuweioptica.com>
2025-04-27 14:04:27 +0800

    add rayleigh sommerfeld

---
+++ include/rayleigh_sommerfeld.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "kernel_loop.hpp"
+namespace kernels
+{
+    template<class T> inline void rayleigh_sommerfeld_kernel(complex_t<T>* p, const vec2<size_t>& shape, const vec2<T>& step, T lambda,T dz)
+    {
+        complex_t<T> ik = 2_PI_I / lambda;
+        T dz_2 = dz * dz;
+        kernels::center_zero_loop_square_r<T, 2>(shape, step, 
+            [&](const vec2<T> fyx, T r_2){
+                T r = std::sqrt(r_2 + dz_2); 
+                complex_t<T> n = 0;
+                if(0 != r){
+                    n = dz * std::exp(ik * r) *(T(1) - ik * r);
+                    n /= (T(2_PI) * std::pow<T>(r, 3));
+                }
+                *p = n;
+                p++;
+            }
+        );
+    }
+}
